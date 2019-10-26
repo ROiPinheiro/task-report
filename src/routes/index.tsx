7595962263448b5a40f3b5c3dbefe31f217bb21f
Rafael Oliveira Pinheiro <38002838+ROiPinheiro@users.noreply.github.com>
2019-10-25 23:18:39 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ const Home = lazy(() => import('../pages/Home'));
 
 export default function Routes() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL + '/'}>
       <Suspense fallback={<h1>Loading...</h1>}>
         <Switch>
           <Route exact path="/">
